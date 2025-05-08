1f9556c73201e069824b4156b87253c7008cad7e
Anuj <anujtiwari4454@outlook.com>
2025-05-08 20:51:57 +0530

    add minor chnage

--- server/server.js
+++ server/server.js
@@ -17,6 +17,11 @@ app.use(bodyParser.json({ limit: "256kb" }));
 
 app.use(compression());
 
+app.use((req, res, next) => {
+  res.set("Cache-Control", "public, max-age=300");
+  next();
+});
+
 app.set("etag", true);
 
 const urls = process.env.URL;
